75b502f5f216dca4ab51babbc6d886d670afc7e0
Joaquin Andresen <joaquin_andresen@icloud.com>
2025-03-22 19:07:15 -0300

    Elimino CLI_LOG_LEVEL de mi-generador.py para obligar a tomar variables de config

--- mi-generador.py
+++ mi-generador.py
@@ -9,7 +9,6 @@ services:
     entrypoint: python3 /main.py
     environment:
     - PYTHONUNBUFFERED=1
-    - LOGGING_LEVEL=DEBUG
     networks:
     - testing_net
     volumes:
@@ -24,7 +23,6 @@ services:
     entrypoint: /client
     environment:
       - CLI_ID={i}
-      - CLI_LOG_LEVEL=DEBUG
     networks:
       - testing_net
     volumes:
