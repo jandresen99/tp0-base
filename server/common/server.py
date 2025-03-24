c0f61341c0bfbd77c89e1eaff65ddb82219ae8e5
Joaquin Andresen <joaquin_andresen@icloud.com>
2025-03-24 14:57:34 -0300

    Ejercicio 8 OK

--- server/common/server.py
+++ server/common/server.py
@@ -1,6 +1,7 @@
 import socket
 import logging
 import signal
+import threading
 from common import utils
 
 
@@ -14,16 +15,10 @@ class Server:
         self.clients = clients
         self.finished_clients = 0
         self.winners = []
+        self.lock = threading.Lock()
+        self.threads = []
 
     def run(self):
-        """
-        Dummy Server loop
-
-        Server that accept a new connections and establishes a
-        communication with a client. After client with communucation
-        finishes, servers starts to accept new connections again
-        """
-
         self.running = True
         signal.signal(signal.SIGTERM, self.shutdown)
 
@@ -31,17 +26,27 @@ class Server:
             try:
                 client_sock = self.__accept_new_connection()
                 if client_sock:
-                    self.__handle_client_connection(client_sock)
+                    client_thread = threading.Thread(target=self.__handle_client_connection, args=(client_sock,))
+                    client_thread.daemon = True
+                    client_thread.start()
+                    self.threads.append(client_thread)
             except:
                 if not self.running:
                     break
+        
+        self.__wait_for_threads()
+    
+    def __wait_for_threads(self):
+        for thread in self.threads:
+            thread.join()
     
     def shutdown(self, signum=None, frame=None):
+        logging.info("action: shutdown | result: in_progress")
         self.running = False
-        logging.info(f'action: shutdown | result: in_progress')
         if self._server_socket:
             self._server_socket.close()
-            logging.info(f'action: shutdown | result: success')
+        self.__wait_for_threads()
+        logging.info("action: shutdown | result: success")
 
     def __handle_client_connection(self, client_sock):
         """
@@ -60,7 +65,8 @@ class Server:
                     if finish:
                         utils.acknowledge_bets(client_sock, bet_count)
                         logging.info(f'action: apuesta_recibida | result: success | cantidad: {bet_count}')
-                        self.finished_clients += 1
+                        with self.lock:
+                            self.finished_clients += 1
                         break
 
                     utils.store_bets(bets)
@@ -69,17 +75,18 @@ class Server:
             if message == "RESULTS":
                 agency_id = utils.receive_message(client_sock)
                 #logging.info(f'action: sorteo | result: pending | finished_clients: {self.finished_clients} | clients: {self.clients} | agency_id: {agency_id}')
-                if int(self.finished_clients) == int(self.clients):
-                    #logging.info(f'action: sorteo | result: in_progress | finished_clients: {self.finished_clients} | clients: {self.clients} | agency_id: {agency_id}')
-                    if not self.winners:
-                        bets = utils.load_bets()
-                        winners = [bet for bet in bets if utils.has_won(bet)]
-                        self.winners = winners
-                        logging.info(f'action: sorteo | result: success')
-                    
-                    agency_winners = [bet for bet in self.winners if bet.agency == int(agency_id)]
+                with self.lock:
+                    if int(self.finished_clients) == int(self.clients):
+                        #logging.info(f'action: sorteo | result: in_progress | finished_clients: {self.finished_clients} | clients: {self.clients} | agency_id: {agency_id}')
+                        if not self.winners:
+                            bets = utils.load_bets()
+                            winners = [bet for bet in bets if utils.has_won(bet)]
+                            self.winners = winners
+                            logging.info(f'action: sorteo | result: success')
+                        
+                        agency_winners = [bet for bet in self.winners if bet.agency == int(agency_id)]
 
-                    utils.send_results(client_sock, agency_winners)
+                        utils.send_results(client_sock, agency_winners)
         except OSError as e:
             logging.error("action: receive_message | result: fail | error: {e}")
         finally:
